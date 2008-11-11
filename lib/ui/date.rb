50bff69709cd6a9791d772d5c236c1cd016ac595
RailsAddict <bauer.mail@gmail.com>
2008-11-10 21:06:22 -0600

    Added Date component

---
+++ lib/ui/date.rb
@@ -0,0 +1,14 @@
+module UI
+  class Date
+
+    def initialize(object, name, field)
+      @object = object
+      @name = name
+      @field = field
+    end
+
+    def to_s
+      Wheels::View.new("ui/date.html.erb", :object => @object, :name => @name, :field => @field).to_s
+    end
+  end
+end
