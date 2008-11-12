d643059c74e51c888c6cd110b5c690162354a991
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-11-12 14:01:09 -0600

    Adding timezones to dates.

--- lib/ui/date_time.rb
+++ lib/ui/date_time.rb
@@ -1,5 +1,17 @@
+require "date"
+require "time"
+
 module UI
   class DateTime
+
+    ZONES = {
+      0  => ["UTC"],
+      -4 => ["EDT"],
+      -5 => ["EST", "CDT"],
+      -6 => ["CST", "MDT"],
+      -7 => ["PDT", "MST"],
+      -8 => ["PST"]
+    }
 
     def initialize(object, name, field)
       @object = object
