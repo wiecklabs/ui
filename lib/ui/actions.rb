a7d51fc6d33516819d6b0672d06c6a6e13000648
Sam Smoot <ssmoot@gmail.com>
2008-12-03 11:00:29 -0600

    Forgot to add the new files.

---
+++ lib/ui/actions.rb
@@ -0,0 +1,26 @@
+module Wheels
+  class ViewContext
+    
+    def actions(name = nil, &b)
+      if name
+        named_actions_captures[name] = capture(&b)
+      else
+        actions_captures << capture(&b)
+      end
+    end
+    
+    def write_actions
+      View.new("ui/actions.html.erb", :content => (actions_captures + named_actions_captures.values).join("\n")).to_s
+    end
+    
+    private
+    def actions_captures
+      @actions_captures ||= []
+    end
+    
+    def named_actions_captures
+      @named_actions_captures ||= {}
+    end
+    
+  end
+end
