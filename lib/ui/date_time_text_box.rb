7ac4795022005dbdfc2f10f76690dffe23e359b8
Sam Smoot <ssmoot@gmail.com>
2008-12-11 17:18:47 -0600

    Use current context.

--- lib/ui/date_time_text_box.rb
+++ lib/ui/date_time_text_box.rb
@@ -1,17 +1,17 @@
 module UI
   class DateTimeTextBox
-    def initialize(object, name, field, options = {})
-       @object = object
-       @name = name
-       @field = field
-       options[:offset] ||= Time.now.gmt_offset / 60 / 60
-       options[:shortcuts] ||= {}
-       @options = options
+    def initialize(context, object, name, field, options = {})
+      @context = context
+      @object = object
+      @name = name
+      @field = field
+      options[:offset] ||= Time.now.gmt_offset / 60 / 60
+      options[:shortcuts] ||= {}
+      @options = options
     end
 
     def to_s
-      context = @options.merge({ :object => @object, :name => @name, :field => @field })
-      Wheels::View.new("ui/date_time_text_box.html.erb", context).to_s
+      @context.render("ui/date_time_text_box.html.erb", { :object => @object, :name => @name, :field => @field }).to_s
     end
   end
 end
