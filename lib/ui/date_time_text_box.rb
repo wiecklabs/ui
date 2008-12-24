56f2584c0ea88be633628fd4277db2a518ca2558
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-12-24 15:47:15 -0600

    Adding UI::DateTimeTextBox::default_offset=

--- lib/ui/date_time_text_box.rb
+++ lib/ui/date_time_text_box.rb
@@ -1,11 +1,20 @@
 module UI
   class DateTimeTextBox
+
+    def self.default_offset=(offset)
+      @default_offset = offset
+    end
+
+    def self.default_offset
+      @default_offset || (Time.now.gmt_offset / 60 / 60)
+    end
+
     def initialize(context, object, name, field, options = {})
       @context = context
       @object = object
       @name = name
       @field = field
-      @offset = options.fetch(:offset, Time.now.gmt_offset / 60 / 60)
+      @offset = options.fetch(:offset, self.class.default_offset)
       @shortcuts = options.fetch(:shortcuts, {})
     end
 
