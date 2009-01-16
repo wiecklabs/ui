7244cf415ef9e59841335900e40916540c3f577f
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-01-16 15:53:49 -0600

    Adding UI::Button

---
+++ lib/ui/button.rb
@@ -0,0 +1,94 @@
+module UI
+  class Button
+
+    def self.public_path=(path)
+      @@public_path = path
+    end
+
+    def self.public_path
+      Pathname(@@public_path).expand_path.to_s
+    rescue NameError
+      raise "UI::Button::public_path not set."
+    end
+
+    attr_accessor :text, :size, :text_color, :button_color, :background_color
+
+    def initialize(text, size = 16, text_color = Color::BLACK, button_color = Color::WHITE)
+      @text = text
+      @size = size
+      @text_color = text_color
+      @button_color = button_color
+    end
+
+    def to_s
+      generate_button unless File.exists?(path)
+      unrooted_path
+    end
+
+    def width
+      ((text.size * size) * 0.55).to_i
+    end
+
+    def height
+      size + 10
+    end
+
+    def unrooted_path
+      "/images/buttons/#{filename}"
+    end
+
+    def filename
+      filename  = text.downcase.gsub(/[^\w]+/, "-")
+      filename << "_#{text_color[1..-1].downcase}_#{button_color[1..-1].downcase}"
+      filename << ".gif"
+    end
+
+    private
+
+    def generate_button
+      FileUtils.mkdir_p(File.dirname(self.class.public_path + unrooted_path))
+      system(convert_command)
+    end
+
+    def path
+      self.class.public_path + unrooted_path
+    end
+
+    def font_path
+      Pathname(__FILE__).dirname.expand_path + "fonts"
+    end
+
+    def convert_command
+
+      cmd = "convert -size #{width}x#{height} "
+
+      # Draw button
+      cmd << "xc:none -fill '#{button_color}' -stroke '#{Color.darken(button_color)}' "
+      cmd << "-draw 'roundRectangle 0, 0, #{width - 4}, #{height - 4}, 4, 4' "
+
+      # Add text
+      cmd << "-stroke none -gravity Center -blur 0x.5 -font #{font_path + "MyriadPro-Semibold.otf"} "
+      cmd << "-pointsize #{size} "
+
+      cmd << "-fill '#{Color.darken(button_color)}' "
+      cmd << "-draw \"text -1, -1 '#{text}'\" "
+      cmd << "-draw \"text -1, 2 '#{text}'\" "
+      cmd << "-draw \"text 1, -1 '#{text}'\" "
+      cmd << "-draw \"text 1, 2 '#{text}'\" "
+
+      cmd << "-fill '#{text_color}' "
+      cmd << "-draw \"text 0, 0 '#{text}'\" "
+
+      cmd << path
+    end
+  end
+end
+
+if __FILE__ == $0
+  require "pathname"
+  require Pathname(__FILE__).dirname + "badge"
+
+  UI::Button.public_path = "/tmp"
+  puts UI::Button.new("Create Candidate", 16, "#559900")
+  `open /tmp/images/buttons/*`
+end
