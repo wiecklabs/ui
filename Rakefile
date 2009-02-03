bb2da3901a9e00eeae2a8cfd1431de4b7c9792d9
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-02-03 11:40:22 -0600

    Adding callback option for live search.

--- Rakefile
+++ Rakefile
@@ -6,7 +6,7 @@ require "rake/gempackagetask"
 
 NAME = "ui"
 SUMMARY = "Wheels UI Components"
-GEM_VERSION = "0.2.6"
+GEM_VERSION = "0.2.6.1"
 
 spec = Gem::Specification.new do |s|
   s.name = NAME
--- lib/ui/live_search_box.rb
+++ lib/ui/live_search_box.rb
@@ -1,12 +1,13 @@
 module UI
   class LiveSearchBox
-    def initialize(context, list_id)
+    def initialize(context, list_id, callback = nil)
       @context = context
       @list_id = list_id
+      @callback = callback
     end
 
     def to_s
-      Wheels::View.new("ui/live_search_box", @context.merge(:list_id => @list_id)).to_s
+      @context.render "ui/live_search_box", :list_id => @list_id, :callback => @callback
     end
   end
 end
