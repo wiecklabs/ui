6d7ae233f6172aa17d1555864a537029e553645a
Bernerd Schaefer <bj.schaefer@gmail.com>
2009-04-06 17:55:49 -0500

    Forgot to to_s layout.

--- lib/ui/javascripts/default_inputs.rb
+++ lib/ui/javascripts/default_inputs.rb
@@ -2,7 +2,7 @@ module UI
   module Javascripts
     class DefaultInputs
       def to_s
-        Wheels::View.new("ui/javascripts/default_inputs")
+        Wheels::View.new("ui/javascripts/default_inputs").to_s
       end
     end
   end
