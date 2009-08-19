984e36ade3a3866f40d65ade5542057c5fba1754
Mike <mike@Mikes-MacBook-Pro.local>
2009-08-19 12:50:30 -0500

    some tweaks to the rich text area widget

--- lib/ui/rich_text_area.rb
+++ lib/ui/rich_text_area.rb
@@ -1,17 +1,18 @@
 module UI
   class RichTextArea
     Asset.register("javascripts/tiny_mce", UI::asset_path + "javascripts/tiny_mce")
-    attr_accessor :advanced, :options
     
-    def initialize(context, width="500", height="100", element_id=nil)
+    def initialize(context, name, width="500", height="100", existing_value="", element_id=nil)
       @context = context
       @width = width
       @height = height
       @element_id = element_id
+      @name = name
+      @existing_value = existing_value
     end
     
     def to_s
-      @context.render("ui/rich_text_area.html.erb", :width => @width, :height => @height, :element_id => @element_id)
+      @context.render("ui/rich_text_area.html.erb", :width => @width, :height => @height, :name => @name, :existing_value => @existing_value, :element_id => @element_id).to_s
     end
     
   end
