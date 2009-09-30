791bcd2340828bdeb99375b9915a6597967eab59
Nicholas <nicholas.d.phillips@gmail.com>
2009-09-30 14:57:54 -0500

    Admin's openModal needed some code to work. Though, I think a standard method of making modals convienent and consistent is needed. Not so sure this is it.

--- assets/javascripts/admin.js
+++ assets/javascripts/admin.js
@@ -1,6 +1,42 @@
 function openModal(content) {
   content.modal();
-  $(window).keyup(function(e) { if ( e.keyCode == 27 ) closeModal(); });
+
+  var height = $(window).height();
+  var width = $(document).width()
+
+  if (arguments.length >= 2) {
+    if (arguments[1] >= 1) {
+      var containerHeight = arguments[1];
+    } else {
+      var containerHeight = height * arguments[1];
+    }
+  } else {
+    var containerHeight = height * 0.7;
+  }
+  
+  var containerTop = ($.browser.msie && $.browser.version < 7 ? $(window).scrollTop() : 0) + (height / 2) - (containerHeight / 2);
+
+  if (containerTop < 1) { containerTop = 1 }
+
+  $("#modalContainer").css("height", containerHeight + "px")
+  $('#modalContainer').css("top", containerTop + "px");
+  
+  if (arguments.length >= 3) {
+    if (arguments[2] >= 1) {
+      var containerWidth = arguments[2];
+    } else {
+      var containerWidth = width * arguments[2] ;
+    }
+  } else {
+    var containerWidth = 770;
+  }
+  
+  var containerLeft = (width / 2) - (containerWidth / 2);  
+    
+  $("#modalContainer").css("width", containerWidth + "px")
+  $('#modalContainer').css("left", containerLeft + "px");  
+    
+  $(document).keyup(function(e) { if ( e.keyCode == 27 ) closeModal(); });
 }
 
 function closeModal() {
--- lib/ui/version.rb
+++ lib/ui/version.rb
@@ -1,3 +1,3 @@
 module UI
-  VERSION = "0.7.6"
+  VERSION = "0.7.7"
 end
