01d843ee186d00d29835e8a116bf63887cf60e13
Nicholas <nicholas.d.phillips@gmail.com>
2009-10-16 11:18:04 -0500

    Added a Date builder to DateTextBox

--- lib/ui/date_text_box.rb
+++ lib/ui/date_text_box.rb
@@ -2,6 +2,14 @@ module UI
   class DateTextBox
     Asset::register("javascripts/dateparse.js", UI::asset_path + "javascripts/dateparse.js")
 
+    def self.build(date_options)
+      if date_options && date_options.is_a?(Array) && !date_options[0].blank?
+        ::Date.civil(*date_options.map { |component| component.to_i })
+      else
+        nil
+      end
+    end
+
     def initialize(context, object, name, field, options = {})
       @context = context
       @object = object
