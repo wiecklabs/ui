84a1d4fe3558fc556f699ecee5f32436dc98669d
Nicholas <nicholas.d.phillips@gmail.com>
2009-10-29 16:34:24 -0500

    Register jquery date picker.

--- lib/ui/assets.rb
+++ lib/ui/assets.rb
@@ -5,6 +5,7 @@ module UI
   Asset::register("javascripts/admin.js", UI::asset_path + "javascripts/admin.js")
   Asset::register("javascripts/default.js", UI::asset_path + "javascripts/default.js")
   Asset::register("javascripts/date.js", UI::asset_path + "javascripts/date.js")
+  Asset::register("javascripts/jquery.datePicker.js", UI::asset_path + "javascripts/jquery.datePicker.js")
   Asset::register("javascripts/DD_belatedPNG_0.0.8a-min.js", UI::asset_path + "javascripts/DD_belatedPNG_0.0.8a-min.js")
   Asset::register("javascripts/jquery.js", UI::asset_path + "javascripts/jquery-1.3.2.min.js")
   
