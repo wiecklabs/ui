647947c981d357d7f55750a1b7b0eaf4d6f2dfc9
Chris Gibson <chris@wieck.com>
2010-06-28 23:27:20 -0500

    Updating some outdated tests

--- test/locale_flag_test.rb
+++ test/locale_flag_test.rb
@@ -2,24 +2,24 @@ require "pathname"
 require Pathname(__FILE__).dirname + "test_helper"
 
 class LocaleFlagTest < Test::Unit::TestCase
-    
+
   def setup
-    
+
   end
-  
+
   def test_unknown_is_returned_when_locale_is_not_found
     flag = UI::LocaleFlag.new("foo")
-    assert_equal "/images/locales/unknown.gif", flag.to_s
+    assert_equal "/assets/images/locales/unknown.gif", flag.to_s
   end
-  
+
   def test_with_locale_to_s
     flag = UI::LocaleFlag.new(Harbor::Locale["en-US"])
-    assert_equal "/images/locales/en-US.gif", flag.to_s
+    assert_equal "/assets/images/locales/en-US.gif", flag.to_s
   end
-  
+
   def test_with_string_to_s
     flag = UI::LocaleFlag.new("en-US")
-    assert_equal "/images/locales/en-US.gif", flag.to_s
+    assert_equal "/assets/images/locales/en-US.gif", flag.to_s
   end
-  
+
 end
